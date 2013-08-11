d33570ee940a24befdb8e6ec0f8d073dd06ab6d2
Santanu <santattech@gmail.com>
2013-08-12 00:12:19 +0530

    Initialize commit with login logout functionality

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FamilyExp::Application.config.secret_key_base = '04dee0f87f3297891d4b24c1d2052eb341ffb0cddaf63ed46df681950314b36c39597982c36c321755886b7b19b45008dfe4a67995bf4f02a9b6e7f4721448db'
